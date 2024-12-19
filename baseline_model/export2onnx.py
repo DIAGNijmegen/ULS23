cb7c2168d3048a007291888a946dc7c11e691445
Max de Grauw <35668031+MJJdG@users.noreply.github.com>
2024-12-19 14:31:47 +0100

    Added working export2onnx script

--- baseline_model/export2onnx.py
+++ baseline_model/export2onnx.py
@@ -1,43 +1,50 @@
-import os
+import argparse
 import torch
-from torch import onnx as onnx
-import numpy as np
+from pathlib import Path
 from nnunetv2.inference.predict_from_raw_data import nnUNetPredictor
 
-# Build the algorithm docker container first using '. build.sh' then run this command
-# docker run -it --entrypoint bash -v $(pwd):/output uls23:latest
-# Now start this script from the container: python3.9 export2onnx.py
+def convert_nnunet_model_to_onnx(predictor):
+    net = predictor.network
+
+    # IMPORTANT: Actually load the data from the loaded checkpoint
+    # in list_of_parameters (only one fold used, so first element)
+    print("Loading weights...", flush=True)
+    net.load_state_dict(predictor.list_of_parameters[0])
+
+    dummy_input = torch.randn(1, 1, 128, 256, 256, requires_grad=True)
+
+    # Set to eval mode
+    net.eval()
+
+    print("Export start...", flush=True)
+    onnx_out = torch.onnx.dynamo_export(net, dummy_input)
+    onnx_out.save("ULS23_test.onnx")
+    print("Export done", flush=True)
 
 if __name__ == "__main__":
-    print("loading model...")
+    # Note: ensure upgraded libraries in docker image:
+    # pip3 install --upgrade torch onnx onnxruntime onnxscript torchvision numpy scipy protobuf
+
+    device = torch.device('cpu')
+    print("Using", device)
+
+    # Initialize the predictor
     predictor = nnUNetPredictor(
         tile_step_size=0.5,
         use_gaussian=True,
         use_mirroring=False,
-        device=torch.device("cpu"),
+        device=device,
         verbose=False,
         verbose_preprocessing=False,
         allow_tqdm=False
     )
-    # Initialize the network architecture, loads the checkpoint of the model you want to export
+
+    print("Loading checkpoint...", flush=True)
+    # Initialize the network architecture, loads the checkpoint of the model you want to export, but doesn't load it to the model yet!
     predictor.initialize_from_trained_model_folder(
-        "/opt/algorithm/nnunet/nnUNet_results/Dataset400_FSUP_ULS/nnUNetTrainer_ULS_500_QuarterLR__nnUNetPlansNoRs__3d_fullres_resenc",
+        r"/input/challenge/experiments/nnUNet_results/Dataset400_FSUP_ULS/nnUNetTrainer_ULS_500_QuarterLR__nnUNetPlansNoRs__3d_fullres_resenc",
         use_folds=("all"),
         checkpoint_name="checkpoint_final.pth",
     )
 
-    # Retrieve the model and set it to eval mode
-    model = predictor.network
-    print(model)
-
-    # We add a (1, 1,) to the shape here to function as a channel dimension and batch size
-    patch_size = (1, 1, 128, 256, 256)
-
-    # Generate some random input data
-    dummy_input = np.random.randn(*patch_size)
-
-    # Export the model
-    print("Saving model...")
-    os.makedirs("/output/onnx_model/")
-    torch.onnx.export(model, torch.from_numpy(dummy_input).type(torch.float), "/output/onnx_model/ULS_nnUnet.onnx", export_params=True, verbose=True)
-    print("Finished :)")
+    convert_nnunet_model_to_onnx(predictor)
