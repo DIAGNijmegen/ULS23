ea5fc5a15d8b3e14487fba71fdd160e10cf71438
Suraj Pai <b.pai@maastrichtuniversity.nl>
2025-05-01 17:05:27 -0400

    Update data_download.sh

--- data_processing/data_download.sh
+++ data_processing/data_download.sh
@@ -6,21 +6,21 @@
 # Set the ZENODO_API_TOKEN environment variable
 # 
 # Part 1
-zenodo-download 10035160 -t $ZENODO_API_TOKEN  -w 8 -d 
+zenodo-download 10035160 -t $ZENODO_API_TOKEN  -w 8 -d .
 
 # Part 2
-zenodo-download 10050960 -t $ZENODO_API_TOKEN  -w 8 -d 
+zenodo-download 10050960 -t $ZENODO_API_TOKEN  -w 8 -d .
 
 # Part 3
-zenodo-download 10054306 -t $ZENODO_API_TOKEN  -w 8 -d 
+zenodo-download 10054306 -t $ZENODO_API_TOKEN  -w 8 -d .
 
 # Part 4
-zenodo-download 10054702 -t $ZENODO_API_TOKEN  -w 8 -d 
+zenodo-download 10054702 -t $ZENODO_API_TOKEN  -w 8 -d .
 
 # Part 5
-zenodo-download 10057471 -t $ZENODO_API_TOKEN  -w 8 -d 
+zenodo-download 10057471 -t $ZENODO_API_TOKEN  -w 8 -d .
 
 # Part 6
-zenodo-download 10056235 -t $ZENODO_API_TOKEN  -w 8 -d 
+zenodo-download 10056235 -t $ZENODO_API_TOKEN  -w 8 -d .
 
 echo -e "\nDownload completed!"
